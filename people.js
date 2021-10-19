a95169606912be76413d02e72e813de8df571a0b
Garyschwartz617 <gary.s.schwartz617@gmail.com>
2021-10-19 23:14:35 +0300

    os and import

---
+++ people.js
@@ -0,0 +1,7 @@
+const people = ['Gary','Adina']
+const ages = [20,15,12,23]
+console.log(people)
+
+module.exports = {
+    people, ages
+}
--- test.js
+++ test.js
@@ -1,3 +1,8 @@
 const nam = 'mario';
 
 console.log(nam);
+
+const greet = (name) => {
+    console.log(`hello, ${name}`)
+}
+greet('mario')
