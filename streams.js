401332adec4ccdfe554b2af66bbc7b9fcfb012d2
Garyschwartz617 <gary.s.schwartz617@gmail.com>
2021-10-20 23:08:58 +0300

    read write make file piping and streaming

---
+++ streams.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+const readStream = fs.createReadStream('./docs/blogs3.txt', {encoding : 'utf-8'})
+const writeStream = fs.createWriteStream('./docs/blogs4.txt')
+
+
+// readStream.on('data', (chunk)=>{
+//     console.log('new chunk')
+//     console.log(chunk)
+//     writeStream.write('\n NEW CHUNK \n')
+//     writeStream.write(chunk)
+// })
+
+// piping
+readStream.pipe(writeStream)
