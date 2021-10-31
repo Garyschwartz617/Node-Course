d84e8710f9590b575314228c46457833fb34391f
Garyschwartz617 <gary.s.schwartz617@gmail.com>
2021-10-31 22:20:18 +0200

    middlewear

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const morgan = require('morgan')
+
 
 const app = express();
 
@@ -6,6 +8,21 @@ app.set('view engine', 'ejs')
 // app.set('set', 'myviews')
 
 app.listen(3000);
+
+app.use(morgan('dev'))
+
+// app.use((req,res,next) => {
+//     console.log('new request made:');
+//     console.log('host: ', req.hostname);
+//     console.log('path: ', req.path);
+//     console.log('method: ', req.method);
+//     next();
+// })
+
+app.use((req,res,next) => {
+    console.log('next middlewear');
+    next();
+})
 
 app.get('/',(req,res) => {
     const blogs = [
